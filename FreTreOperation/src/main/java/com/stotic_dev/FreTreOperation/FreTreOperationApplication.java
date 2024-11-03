7c0ae29bf762beadea8fabb07aa3f497b49c9ca6
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-11-03 12:51:43 +0900

    Initial Commit

---
+++ FreTreOperation/src/main/java/com/stotic_dev/FreTreOperation/FreTreOperationApplication.java
@@ -0,0 +1,13 @@
+package com.stotic_dev.FreTreOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreTreOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreTreOperationApplication.class, args);
+	}
+
+}
---
+++ FreTreOperation/src/test/java/com/stotic_dev/FreTreOperation/FreTreOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stotic_dev.FreTreOperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreTreOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
